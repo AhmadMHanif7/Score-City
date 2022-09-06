7d9d21edc527a4c271f694e3c83b2524e0353d75
Ahmad Hanif <ahmadmhanif@Ahmads-MBP.lan>
2022-09-06 18:56:46 -0500

    League Standings and Statistics

--- standings.js
+++ standings.js
@@ -1,9 +1,11 @@
 const axios = require("axios");
 
+var leagueStandings = [];
+
 const options = {
   method: 'GET',
   url: 'https://api-football-beta.p.rapidapi.com/standings',
-  params: {season: '2022', league: '140'},
+  params: {season: '2022', league: '39'},
   headers: {
     'X-RapidAPI-Key': 'ee03341b2amsh862838b860df93fp1c2c67jsn507296649d6f',
     'X-RapidAPI-Host': 'api-football-beta.p.rapidapi.com'
@@ -11,7 +13,27 @@ const options = {
 };
 
 axios.request(options).then(function (response) {
-	console.log(response.data.response[0].league.standings);
+
+  for (let i = 0; i < response.data.response[0].league.standings[0].length; i++) {
+    let positions = {
+      'rank': response.data.response[0].league.standings[0][i].rank,
+      'name': response.data.response[0].league.standings[0][i].team.name,
+      'logo': response.data.response[0].league.standings[0][i].team.logo,
+      'games played' : response.data.response[0].league.standings[0][i].all.played,
+      'wins' : response.data.response[0].league.standings[0][i].all.win,
+      'draws': response.data.response[0].league.standings[0][i].all.draw,
+      'loses': response.data.response[0].league.standings[0][i].all.lose,
+      'points': response.data.response[0].league.standings[0][i].points,
+      'goals for': response.data.response[0].league.standings[0][i].all.goals.for,
+      'goals against': response.data.response[0].league.standings[0][i].all.goals.against,
+      'goal differential': response.data.response[0].league.standings[0][i].goalsDiff,
+      'recent form' : response.data.response[0].league.standings[0][i].form
+    }
+
+    leagueStandings.push(positions)
+  }
+  
+	console.log(leagueStandings);
 }).catch(function (error) {
 	console.error(error);
 });
--- topscorer.js
+++ topscorer.js
@@ -1,9 +1,11 @@
 const axios = require("axios");
 
+var playerStats = []
+
 const options = {
   method: 'GET',
   url: 'https://api-football-beta.p.rapidapi.com/players/topscorers',
-  params: {season: '2020', league: '140'},
+  params: {season: '2021', league: '140'},
   headers: {
     'X-RapidAPI-Key': 'ee03341b2amsh862838b860df93fp1c2c67jsn507296649d6f',
     'X-RapidAPI-Host': 'api-football-beta.p.rapidapi.com'
@@ -11,7 +13,19 @@ const options = {
 };
 
 axios.request(options).then(function (response) {
-	console.log(response.data.response);
+  
+  for (let i = 0; i < 10; i++){
+    let position = {
+      'name' : response.data.response[i].player.name,
+      'photo' : response.data.response[i].player.photo,
+      'team' : response.data.response[i].statistics[0].team.name,
+      'goals' : response.data.response[i].statistics[0].goals.total,
+      'assists' : response.data.response[i].statistics[0].goals.assists
+    };
+    playerStats.push(position)
+  }
+
+	console.log(playerStats);
 }).catch(function (error) {
 	console.error(error);
 });
