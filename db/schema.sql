2f6454cfc8801895fe3464a08bde76d918f9f13a
Ahmad Hanif <ahmadmhanif@Ahmads-MBP.lan>
2022-09-07 14:54:55 -0500

    Update database

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE user_info (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL,
-    email VARCHAR(30) NOT NULL
+    email VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,5 @@
-INSERT INTO user_info (username, email)
+INSERT INTO user_info (username, email, password)
 VALUES
-('Ahmad1', 'ahmadmhanif7@gmail.com'),
-('Jake124', 'jake124@gmail.com'),
-('L3Dgo', 'l3dgo@gmail.com');
+('Ahmad1', 'ahmadmhanif7@gmail.com', '1234'),
+('Jake124', 'jake124@gmail.com', '5678'),
+('L3Dgo', 'l3dgo@gmail.com', '90123');
