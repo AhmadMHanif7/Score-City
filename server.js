0a3cadf8521e32221be2046d863620e002b9f64d
Ahmad Hanif <ahmadmhanif@Ahmads-MBP.lan>
2022-09-07 14:33:08 -0500

    model and database

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: '127.0.0.1',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Realmadrid1!',
+      database: 'users'
+    },
+    console.log('Connected to the election database.')
+  );
+
+db.query(`SELECT * FROM user_info`, (err, rows) => {
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log({rows});
+})
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
