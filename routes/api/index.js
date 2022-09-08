2d35e5344c99591ce810a7079c1d3c2d782ce26f
DavidKD5 <jdbetancourt6@gmail.com>
2022-09-08 13:27:16 -0600

    top scorer view complete

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes.js");
+const dashboardRoutes = require("./dashboard-routes.js");
 
 router.use("/users", userRoutes);
 
